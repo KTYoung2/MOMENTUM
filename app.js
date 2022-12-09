a1b051063071054f5ec6154459c038b5d632245c
kty <xodud9625@hanmail.net>
2022-12-09 21:20:54 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,49 @@
+
+/* 변수 선언과 계산기 만들기
+
+const a = 5;
+const b = 2;
+
+console.log(a+b);
+console.log(a*3);
+
+// 변수값을 바꿀수도 있을 땐 let
+let myName ="young";
+
+console.log("myname" + myName);
+
+myName = "taetae";
+
+console.log("newName" + myName);
+
+// DataType - Boolean
+
+const amIFat = true;
+console.log(amIFat);
+
+//데이터 정리하는 법 array
+
+
+const daysOfWeek = ["mon", "tue", "wed","the", "fri"];
+
+console.log(daysOfWeek[2]);
+
+daysOfWeek.push("sat");
+console.log(daysOfWeek);
+*/
+//objects
+
+const player = {
+    name : "young",
+    points : 8,
+    fat : true,
+};
+
+/*
+player.name = "ktty";
+console.log(player.name);
+*/
+
+player.lastName = "summer";
+console.log(player);
+
