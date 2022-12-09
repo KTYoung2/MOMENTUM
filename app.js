56a01109931bebdd6cddf3d9d7bf2e4a16ba5b83
kty <xodud9625@hanmail.net>
2022-12-09 22:41:58 +0900

    commit

--- app.js
+++ app.js
@@ -32,18 +32,82 @@ daysOfWeek.push("sat");
 console.log(daysOfWeek);
 */
 //objects
-
+/*
 const player = {
     name : "young",
     points : 8,
     fat : true,
 };
 
-/*
+
 player.name = "ktty";
 console.log(player.name);
-*/
+
 
 player.lastName = "summer";
 console.log(player);
+
+*/
+
+//function
+//argument 받는 방식
+function sayHello(nameOfPerson, age) {
+    console.log ("Hello my name is "+ nameOfPerson + " and i'm " + age);
+}
+
+//argument 보내는 방식
+sayHello("young", 26);
+sayHello("winter",1);
+
+
+//계산기 만들기
+            //// function 데이터 받는 방식(firstNum, secondNum)
+function plus(firstNum, secondNum){
+    console.log(firstNum+ secondNum);
+}
+
+plus(5,3);
+// function 데이터 보내기 
+
+function divide (firstNum, secondNum){
+    console.log(firstNum / secondNum);
+}
+
+divide(10,2);
+
+
+const player = {
+    name : "young",
+    sayHello: function(otherPersonName) {
+        console.log("Hello!" + otherPersonName + " Nice to meet you");
+    }
+};
+
+console.log(player.name);
+player.sayHello("winter");
+player.sayHello("shiri");
+
+// 계산기 만들기 (과제)
+
+const caluator = {
+    plus : function (a,b){
+        console.log(a+b);
+    }, 
+    sub : function (a,b){
+        console.log(a-b);
+    },
+    mult : function (a,b){
+        console.log(a * b);
+    },
+    divide : function(a,b){
+        console.log(a / b);
+    }
+
+};
+
+caluator.plus(1,5);
+caluator.sub(10,5);
+caluator.mult(9,50);
+caluator.divide(4,80);
+
 
