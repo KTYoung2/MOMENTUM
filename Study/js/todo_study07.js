2d058186ddae6884e73f3cb0f27481ad041f04be
kty <xodud9625@hanmail.net>
2022-12-22 18:04:26 +0900

    commit

--- Study/js/todo_study07.js
+++ Study/js/todo_study07.js
@@ -4,6 +4,7 @@ const toDoList = document.getElementById("todo-list");
 
 const TODOS_KEY = "todos";
 
+//toDos는 항상 비어 있는 array로 시작 const -> let 변경 왜?? 투두리스트 업데이트가 필요하니까. 
 let toDos = [];   //배열은 localStorge저장 안됨
 
 //투두리스트 저장 함수
@@ -14,22 +15,25 @@ function saveTodos() {
 
 //버튼 투드 리스트 삭제 기능 함수
 function delTodo(event){
-    //버튼을 클릭할때 (event)를 얻게됨 --> event는 target을 줌.
+    //버튼을 클릭할때 (event)를 얻게됨 --> event는 target(클릭된 html element)을 줌.
     //삭제해야할 타켓 지정 
     const li = event.target.parentElement;
+    
     li.remove();
 }
 
 
 //todo List 작성
 function paintToDo(newTodo) {
+    //todolist 사용할 html element 만들기 
    const li = document.createElement("li");
+   li.id = newTodo.id;
    const span = document.createElement("span");
-   span.innerText = newTodo;   
+   span.innerText = newTodo.text;   
    const button = document.createElement("button");
    button.innerText = "❌";
    button.addEventListener("click", delTodo);
-   //li 자식요소 넣어주기
+   //li안에 span, button 자식요소 설정하기
    li.appendChild(span);
    li.appendChild(button);
    toDoList.appendChild(li);
@@ -40,9 +44,14 @@ function toDoSub(event){
     event.preventDefault();
     const newTodo = toDoInput.value; // --> input의 현재 value(값)를 새로운 변수(newTodo)에 복사
     toDoInput.value = "";
-    //newTodo를 toDos array에 push
-    toDos.push(newTodo);
-    paintToDo(newTodo);
+    //text로만 데이터(리스트)를 식별할수 없음(삭제할 때) -> object 저장해 리스트에 식별번호 부여 
+    const newTodoObj = {
+        text:newTodo,
+        id: Date.now(),
+    }; 
+        //newTodo를 toDos array에 push
+    toDos.push(newTodoObj);
+    paintToDo(newTodoObj);
     saveTodos(); //---> newTodo는 array에 들어감
 }
 
@@ -52,9 +61,22 @@ const savedToDos = localStorage.getItem(TODOS_KEY);
 
 //투두리스트 저장 함수
 if(savedToDos !== null ){
-                    //js에서 사용가능한 object
-    const parsedToDos = JSON.parse(savedToDos);
-    toDos = parsedToDos;
-    // array 각 item 하나마다 function 실행을 할 수 있음
+      /* 
+      원래 내가 만든 배열은 string만 나열해둔 단순한 배열이었는데,
+      그 배열을  JSON.parse 함수를 이용하면 
+      js가 이해할 수 있는, 살아있는(실제로 무언갈 할 수 있는 ) array로 만들 수 있음.
+      */
+    const parsedToDos = JSON.parse(savedToDos); 
+    toDos = parsedToDos;  //기존에 있던 리스트(db)를 간직(기억)한 채 새로운 투두리스트(db)추가 
+      // forEach -> array 각 item 하나마다 function 실행을 할 수 있음    
     parsedToDos.forEach(paintToDo);
+    /** arrow function(화살표 함수) 둘이 같음
+     * 
+     * (item) => console.log("this is the turn of : " item )
+     * 
+     * function sayHello(item){
+     * onsole.log("this is the turn of : " item );
+     * }
+     * 
+    */
 }
