edcb238cc6ba371fa0a9590434df51fecbf7c9cd
kty <xodud9625@hanmail.net>
2023-03-06 23:00:50 +0900

    commit

---
+++ review/js02.js
@@ -0,0 +1,12 @@
+
+/*
+   JavaScript를 사용하는 이유는 HTML과 상호작용하기 위함.
+   즉, js를 통해  HTML의  Element들을 변경하고  읽을 수 있음.
+
+   JavaScript로 html의 정보를 가질 수 있는 방법
+   document 객체와 Element를 가져오는 수 많은 함수들을 이용하는 것.
+
+*/
+
+const title = document.getElementById("title");
+
