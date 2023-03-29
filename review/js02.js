e764798fa315602751a768f7707173e29e4e7763
kty <xodud9625@hanmail.net>
2023-03-29 22:06:30 +0900

    commit

--- review/js02.js
+++ review/js02.js
@@ -1,12 +1,15 @@
 
-/*
-   JavaScript를 사용하는 이유는 HTML과 상호작용하기 위함.
-   즉, js를 통해  HTML의  Element들을 변경하고  읽을 수 있음.
+const h1 = document.querySelector(".hello h1");
 
-   JavaScript로 html의 정보를 가질 수 있는 방법
-   document 객체와 Element를 가져오는 수 많은 함수들을 이용하는 것.
 
-*/
+//html에서 설정한 clssName에 js className 추가하기
+function handleTitleClick(){
+   const clickedClass = "clicked";
+   h1.classList.toggle(clickedClass);
+      /* ---> toggle이란 html element h1의 classList에 "clicked" className이 있는지 
+             확인한 후  있다면  clicked remove 없다면 clicked add  
+      */
+}
 
-const title = document.getElementById("title");
 
+h1.addEventListener("click", handleTitleClick);
