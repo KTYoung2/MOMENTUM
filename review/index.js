b99dce801e3dbcee8acac1f9cb79dba10707f464
kty <xodud9625@hanmail.net>
2023-04-16 18:31:27 +0900

    commit

---
+++ review/index.js
@@ -0,0 +1,31 @@
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
+
+function onLoginSub(event) {
+    //모든 EventListener function의 첫번째 인자(argument)는 항상 지금 발생한 이벤트에 대한 (js가 주는)정보가 될것임.
+    event.preventDefault();
+        /*preventDefault(); -> submit은 발생과 동시에 새로고침이 일어나니, 
+          그것을 멈추기 위해 사용.(브라우저의 기본동작 멈춤)
+          why? user 정보를 받고 저장하기 위해 !
+        */
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    //user정보 저장후 loginForm 숨기기. 
+    const userName = loginInput.value;
+    //user정보 저장
+    greeting.innerText = ` Hello ${userName}`;
+    //저장된 user정보 h1에 추가하기. 
+                        /* `${변수명}` 1.변수와 string을 결합하고 싶을때
+                                      2.변수를 string 안에 집어넣고 싶을 때
+                                      ※ '' 싱글코트가 아닌 `` 백틱 기호임  */  
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+    // user정보 저장후 h1 hidden classname 삭제 -> 유저 네임이 나와야 하니까 !
+
+};
+
+
+
+loginForm.addEventListener("submit", onLoginSub);
+                    //submit -> 엔터, 버튼을 클릭할 때 발생
--- review/js02.js
+++ review/js02.js
@@ -13,3 +13,25 @@ function handleTitleClick(){
 
 
 h1.addEventListener("click", handleTitleClick);
+
+
+// 4.0~ 4.1
+
+const loginForm = document.querySelector("#login-form");
+const loginInput = loginForm.querySelector("input");
+const loginButton = loginForm.querySelector("button");
+//document처럼 loginForm 안에 있는 html element 바로 가져올 수 있음. 
+
+
+//user 입력값 유효성 확인
+function handleLoginBtn() {
+    const userName =loginInput.value;
+    if(userName === ""){
+        alert("Please write your name");
+    } else if(userName.length >= 15){
+        //.length => string 길이 구하는 함수
+        alert("Your name is too Long");
+    }
+};
+
+//=====> js만으로 작업해야한다면 필요하겠지만, 브라우저와html에서 제공하는 기능이 있으니 그걸 먼저 활용할것!
