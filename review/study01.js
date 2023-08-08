0d40bbaeb07bbb7f0e1806ed7c975a8d5d68b25e
kty <xodud9625@hanmail.net>
2023-08-08 22:21:18 +0900

    commit

---
+++ review/study01.js
@@ -0,0 +1,26 @@
+//01.변수
+
+//const (상수) :  값이 바뀔 수 없음
+const a = 5;
+const myName = "young";
+
+console.log(a + 2);
+console.log("흔수의 정신적인 나이는 " + a + "다.")
+console.log("my name is " + myName);
+
+//let : 값을 업데이트 할 수 있음
+let myDog = "sunmmer";
+
+console.log(myDog);
+
+//값 업데이트
+myDog = "winter";
+
+console.log(myDog);
+
+/**
+
+기본적으로 const 가 디폴트. 
+업데이트가 필요할 때 가끔 let
+
+ */
